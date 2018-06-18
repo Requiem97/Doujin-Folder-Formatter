269318e0b348cdd8041a3244e6f6725bd1188f0f
Requiem97 <marius.1997@gmail.com>
2018-06-18 23:39:22 +0800

    made script do all functions automatically

--- script.py
+++ script.py
@@ -8,11 +8,14 @@ def remove_json(path):
             if fn == "contentV2.json":
                 os.remove(os.path.join(parent, fn))
 
+
 def rename_thumb(path):
     for parent, sub, filenames in os.walk(path):
         for fn in filenames:
             if fn.lower() == "thumb.jpg" or fn.lower() == "thumb.png":
-                os.rename(os.path.join(parent, fn), os.path.join(parent, "folder.jpg"))
+                os.rename(os.path.join(parent, fn),
+                          os.path.join(parent, "folder.jpg"))
+
 
 def del_duplicate_thumb(path):
     for parent, sub, filenames in os.walk(path):
@@ -20,19 +23,17 @@ def del_duplicate_thumb(path):
             if fn == "thumb.jpg" or fn == "thumb.png":
                 os.remove(os.path.join(parent, fn))
 
+
 if __name__ == "__main__":
-    while True:
-        inp = input("Choose 0 to delete .json, 1 to rename thumb.jpg, 2 delete duplicate thumb, 3 to rename folder, or 4 to exit: ")
-        if inp == '0':
-            remove_json(os.getcwd())
-        elif inp == '1':
-            rename_thumb(os.getcwd())
-            print("success")
-        elif inp == '2':
-            del_duplicate_thumb(os.getcwd())
-        elif inp == '3':
-            rename.rename_folders()
-        elif inp == '4':
-            break
-        else:
-            print("invalid input")
+    print("Deleting contentV2.json")
+    remove_json(os.getcwd())
+    print("success")
+    print("Renaming thumb images to folder.jpg")
+    rename_thumb(os.getcwd())
+    print("success")
+    print("Deleting duplicate thumb file")
+    del_duplicate_thumb(os.getcwd())
+    print("success")
+    print("Reformatting folder names")
+    rename.rename_folders()
+    print("success")
