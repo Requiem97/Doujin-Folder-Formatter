9dfe442ec091e0eb9bd5d9c31a9d0b7fbffb6869
Requiem97 <marius.1997@gmail.com>
2018-06-18 23:10:25 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Doujin Folder Formatter
+Formats folder names from the folders downloaded by Hentoid
+## rename.py Sample Cases
+**Before**
+> Ohtomo_Takuji - Kaikin__Namaiki_Hammann___Unforbidden__Hammann_s_raw_orgasm_______________ - [39031-tsumino]
+> name1_name2 - t_i_t_l_e____ - [id-source]
+> name1_name2 - t_i_t_l_e_-_o_t_h_e_r - [id-source]
+> name1_name2 - name1_name2_t_i_t_l_e_-_o_t_h_e_r - [id-source]
+**After**
+> [name1 name2] title
+> [name1 name2] title - other
+> [name1 name2] title
+
+Script can also automatically handle some posessives
+> \_s\_
+becomes
+> 's 
+> \_t\_
+becomes
+> 't 
+> \_ve\_
+becomes
+> 've
+Other possesives will be added if needed
---
+++ rename.py
@@ -0,0 +1,43 @@
+import os
+import re
+
+def rename_folders():
+    base_dir = os.getcwd()
+    dj = os.listdir(base_dir)
+    new_dj = None
+    print()
+    exclude_files = ["__pycache__","backup.txt", "rename.py", "script.py"]
+    for file_name in exclude_files:
+        try:
+            dj.remove(file_name)
+        except Exception:
+            print("No File: "+file_name+" found")
+            continue
+    print(dj)
+    
+    backup = open("backup.txt", "w")
+    for d in dj:
+        new_dj = (d.split(" - "))
+        backup.write(d + "\n")
+        if len(new_dj) < 3:
+            if len(new_dj) == 1:
+                author = new_dj[0].replace("[",'').split("]")
+                folder_name = "["+author[0]+"]"+" "+ author[-1].replace(author[0], '').strip()
+                print(folder_name)
+                os.rename(os.path.join(base_dir, d), os.path.join(base_dir, folder_name))
+                continue
+            else:
+                continue
+        else:
+            author = "[" + new_dj[0].replace("_", " ").title() + "]"
+            title = new_dj[1].strip()
+            title = title.replace("_s_", "'s_").replace("'t_", "'t ").replace("_ve_", "'ve ")
+            title = title.replace("_", " ")
+            title = title.title().replace("Fakku", '').replace(new_dj[0], '')
+            title = title.replace("'S", "'s").replace("'T", "'t").replace("'Ve", "'ve")
+            folder_name = re.sub(' +',' ', author + " " + title)
+            os.rename(os.path.join(base_dir, d), os.path.join(base_dir, folder_name))
+            continue
+
+if __name__ == "__main__":
+    rename_folders()
